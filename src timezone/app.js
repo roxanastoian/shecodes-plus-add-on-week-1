99a417356119b1fa1653ac836784245cfb16200d
roxanastoian <131897578+roxanastoian@users.noreply.github.com>
2023-06-17 21:38:50 +0100

    Corrected mistakes

--- src timezone/app.js
+++ src timezone/app.js
@@ -2,29 +2,15 @@
 //Asia / Tokyo;
 //Australia / Sydney;
 
-function alertTimezone(event) {
-  let franceElement = moment()
-    .tz("Europe / Paris")
-    .format("dddd, MMMM D, YYYY HH:mm A");
-  let tokyoElement = moment()
-    .tz("Asia / Tokyo")
-    .format("dddd, MMMM D, YYYY HH:mm A");
-  let sydneyElement = moment()
-    .tz("Australia / Sydney")
-    .format("dddd, MMMM D, YYYY HH:mm A");
+function showDate(event) {
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
 
-  if (event.target.value === "france") {
-    alert(`It is ${franceElement} in Europe/Paris.`);
-  }
-  if (event.target.value === "tokyo") {
-    alert(`It is ${tokyoElement} in Asia / Tokyo.`);
-  }
-  if (event.target.value === "sydney") {
-    alert(`It is ${sydneyElement} in Australia / Sydney.`);
+    alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
 
-let countriesSelect = document.querySelector("#countries");
-countriesSelect.addEventListener("change", alertTimezone);
-
-// It is Saturday, October 8, 2023 2:51 PM in Europe/Paris
+let selectElement = document.querySelector("#city");
+selectElement.addEventListener("change", showDate);
